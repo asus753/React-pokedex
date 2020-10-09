9412727ba8fde647799f1c6b5b7dd536cf228c00
maurperez <mauro.alejandro.perez@outlook.es>
2020-10-09 14:56:49 -0300

    cleans generation components

--- src/components/generation/Generation.jsx
+++ src/components/generation/Generation.jsx
@@ -16,57 +16,57 @@ export const Generation = () => {
   if(data){
     const generation = mapearGeneration(data)
 
-    return <div style={{textAlign : 'center'}}>
-      <h1>{generation.name} ({generation.id})</h1>
-      <Card border='info' style={{margin : '1rem'}}>
+    return <div>
+      <h1 className="text-center">{generation.name} ({generation.id})</h1>
+      <Card border='info' className="m-1 text-center">
         <Card.Body>Main region : <strong>{generation.mainRegion}</strong></Card.Body>
       </Card>
       <Accordion>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey='0'>Pokemons species added</Accordion.Toggle>
+          <Accordion.Toggle as={Card.Header} eventKey='0' className="text-center">Pokemons species added</Accordion.Toggle>
           <Accordion.Collapse eventKey='0'>
             <Card.Body>
-              <ul style={{textAlign : 'initial', columns : '2'}}>
+              <ul style={{columns : '2'}}>
                 {generation.pokemons.map((pokemon, idx) => <li key={idx}><strong>{pokemon}</strong></li>)}
               </ul>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey='1'>Moves added</Accordion.Toggle>
+          <Accordion.Toggle as={Card.Header} eventKey='1' className="text-center">Moves added</Accordion.Toggle>
           <Accordion.Collapse eventKey='1'>
             <Card.Body>
-              <ul style={{textAlign : 'initial', columns : '2'}}>
+              <ul style={{columns : '2'}}>
                 {generation.moves.map((move, idx) => <li key={idx}><Link to={`/move/${move}`}><strong>{move}</strong></Link></li>)}
               </ul>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey='2'>Abilities added</Accordion.Toggle>
+          <Accordion.Toggle as={Card.Header} eventKey='2' className="text-center">Abilities added</Accordion.Toggle>
           <Accordion.Collapse eventKey='2'>
             <Card.Body>
-              <ul style={{textAlign : 'initial', columns : '2'}}>
+              <ul style={{columns : '2'}}>
                 {generation.abilities.map((ability, idx) => <li key={idx}><Link to={`/ability/${ability}`}><strong>{ability}</strong></Link></li>)}
               </ul>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey='3'>Moves added</Accordion.Toggle>
+          <Accordion.Toggle as={Card.Header} eventKey='3' className="text-center">Moves added</Accordion.Toggle>
           <Accordion.Collapse eventKey='3'>
             <Card.Body>
-              <ul style={{textAlign : 'initial', columns : '2'}}>
+              <ul style={{columns : '2'}}>
                 {generation.moves.map((move, idx) => <li key={idx}><Link to={`/move/${move}`}><strong>{move}</strong></Link></li>)}
               </ul>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey='4'>Types added</Accordion.Toggle>
+          <Accordion.Toggle as={Card.Header} eventKey='4' className="text-center">Types added</Accordion.Toggle>
           <Accordion.Collapse eventKey='4'>
             <Card.Body>
-              <ul style={{textAlign : 'initial', columns : '2'}}>
+              <ul style={{columns : '2'}}>
                 {generation.types.map((type, idx) => <li key={idx}><Link to={`/type/${type}`}><strong>{type}</strong></Link></li>)}
               </ul>
             </Card.Body>
