a73b08c101fda06bb91acc47b658e575ab0f5e7c
maurperez <mauro.alejandro.perez@outlook.es>
2020-10-10 15:02:12 -0300

    move now is reponsive

--- src/components/move/Move.jsx
+++ src/components/move/Move.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import {useFetchReducer} from '../../hooks/useFetchWithCache.js'
 import pokeAPI from '../../pokeAPI.js'
 import { Loading } from '../general/Loading.jsx'
-import {Alert, Card} from 'react-bootstrap'
+import {Alert, Card, Row, Col} from 'react-bootstrap'
 import { mapearMove } from '../../mappers/move.js'
 
 export const Move = () => {
@@ -22,27 +22,31 @@ export const Move = () => {
       <div className="text-center">
         <h1>{move.name} ({move.id})</h1>
         <p>{move.description.text} <br></br><small>(version : {move.description.version})</small></p>
-        <div className="d-flex">
-          <Card className="mr-3 w-25" border='info'>
-            <Card.Body>
-              <Card.Title>Move stats</Card.Title>
-              <ul className="text-left">
-                <li><strong>Damage class: </strong>{move.damageClass}</li>
-                <li><strong>Type: </strong>{move.type}</li>
-                <li><strong>Accuracy: </strong>{move.accuracy}</li>
-                <li><strong>PP: </strong>{move.pp}</li>
-                <li><strong>Power: </strong>{move.power}</li>
-              </ul>
-            </Card.Body>
-          </Card>
-          <Card border='info' className="w-75">
-            <Card.Body>
-              <Card.Title>Description</Card.Title>
-              <p>This movement was introduced in <strong>"{move.generation}"</strong></p>
-              <p><strong>Effect description: </strong>{move.effect}</p>
-            </Card.Body>
-          </Card>
-        </div>
+        <Row>
+          <Col md={4} className='mt-3'>
+            <Card border='info' className='h-100'>
+              <Card.Body>
+                <Card.Title>Move stats</Card.Title>
+                <ul className="text-left">
+                  <li><strong>Damage class: </strong>{move.damageClass}</li>
+                  <li><strong>Type: </strong>{move.type}</li>
+                  <li><strong>Accuracy: </strong>{move.accuracy}</li>
+                  <li><strong>PP: </strong>{move.pp}</li>
+                  <li><strong>Power: </strong>{move.power}</li>
+                </ul>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md className='mt-3'>
+            <Card border='info' className='h-100'>
+              <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <p>This movement was introduced in <strong>"{move.generation}"</strong></p>
+                <p><strong>Effect description: </strong>{move.effect}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </div>
     )
   }
