0dd1361c99d9b8738716255c28c88d9c910f4743
maurperez <mauro.alejandro.perez@outlook.es>
2020-10-14 14:30:39 -0300

    leastest small corrections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-//import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
 import { App } from './components/App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
