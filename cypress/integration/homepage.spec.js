2d2b21451f1549df9c8730040a319c46ef82253c
maurperez <mauro.alejandro.perez@outlook.es>
2020-10-28 14:06:43 -0300

    adds new test to the homepage to verify that the buttons redirects correctly

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -11,7 +11,7 @@ describe('homepage', () => {
       cy.get('#main-buttons').should('be.visible')
     })
 
-    it('renders the buttons with the corresponding href', () => {
+    it('renders the links as buttons with the corresponding href', () => {
 
       cy.get('#main-buttons').contains('Pokemons by name').should('be.visible')
       cy.get('#main-buttons').contains('Pokemons by name').parent('a')
@@ -32,6 +32,49 @@ describe('homepage', () => {
       cy.get('#main-buttons').contains('Generations').should('be.visible')
       cy.get('#main-buttons').contains('Generations').parent('a')
         .should('have.attr', 'href').and('eq', '/generation')
+
+    })
+
+    context('the buttons redirects correctly to the corresponding page', () => {
+
+      beforeEach(() => {
+        cy.visit(Cypress.config('baseUrl'))
+      })
+
+      it('"Pokemons by name" redirects to the pokemons list', () => {
+        cy.stubPokemonsList()
+        cy.get('#main-buttons').contains('Pokemons by name').parent('a').click()
+
+        cy.url().should('eq', Cypress.config('baseUrl') + '/pokemon')
+      })
+
+      it('"Pokemons by type" redirects to the types list', () => {
+        cy.stubTypesList()
+        cy.get('#main-buttons').contains('Pokemons by type').parent('a').click()
+
+        cy.url().should('eq', Cypress.config('baseUrl') + '/type')
+      })
+
+      it('"Movements" redirects to the movements list', () => {
+        cy.stubMovesList()
+        cy.get('#main-buttons').contains('Movements').parent('a').click()
+
+        cy.url().should('eq', Cypress.config('baseUrl') + '/move')
+      })
+
+      it('"Abilities" redirects to the abilities list', () => {
+        cy.stubAbilitiesList()
+        cy.get('#main-buttons').contains('Abilities').parent('a').click()
+
+        cy.url().should('eq', Cypress.config('baseUrl') + '/ability')
+      })
+
+      it('"Generations" redirects to the abilities list', () => {
+        cy.stubGenerationsList()
+        cy.get('#main-buttons').contains('Generations').parent('a').click()
+
+        cy.url().should('eq', Cypress.config('baseUrl') + '/generation')
+      })
 
     })
   })
